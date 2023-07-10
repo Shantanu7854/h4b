eda3bac381d50ebc034aff32234da8e44d027dbf
Shantanu Saha <shantanusaha677@gmail.com>
2023-07-10 17:42:02 +0530

    client code updated

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,50 @@
+import { useCallback, useEffect, useState } from "react";
 import CreateButton from "./CreateButton";
+import Product from "./Product";
 
 const Dashboard = () => {
+  const [productDetails, setProductDetails] = useState([]);
+  const [error, setError] = useState(null);
+
+    useEffect (
+      () => {
+        const fetchDataHandler = 
+        async () => {
+          // try {
+            const response = await fetch('https://geeky-nerds-default-rtdb.firebaseio.com/data.json')
+            if(!response.ok) {
+              throw new Error('Something went wrong!!!')
+            }
+            const responseData = await response.json();
+    
+            // console.log(responseData);
+
+            const loadedData = [];
+            for(const key in responseData) {
+              loadedData.push(
+                {
+                  id: key,
+                  product_name: responseData[key].product_name,
+                  owner_name: responseData[key].owner_name,
+                  owner_email: responseData[key].owner_email,
+                  image_link: responseData[key].image_link,
+                  description: responseData[key].description,
+                  sell_value: responseData[key].sell_value,
+                  buy_value: responseData[key].buy_value,
+                }
+              );
+            }
+            setProductDetails(loadedData)
+          // } catch(error) {
+          //   setError(error.message);
+          // }
+        }
+
+        fetchDataHandler();
+      }, []);
+
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <section id="trade" className="bg-white dark:bg-gray-900">
       <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
         <div className="max-w-screen-md mb-8 lg:mb-16">
           <h2 className="mb-4 text-4xl tracking-tight font-bold text-gray-900 dark:text-white">
@@ -13,130 +55,24 @@ const Dashboard = () => {
             and capital can unlock long-term value and drive economic growth.
           </p>
         </div>
-        <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
-          <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
-              <img
-                className="p-8 rounded-t-lg"
-                src="https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
-                alt="product image"
-              />
-            </a>
-            <div className="px-5 pb-5">
-              <a href="#">
-                <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                  Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
-                </h5>
-              </a>
-              <div className="flex items-center mt-2.5 mb-5"></div>
-              <div className="flex items-center justify-between">
-                <a
-                  href="#"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Buy
-                </a>
-                <span className="text-3xl font-bold text-gray-900 dark:text-white">
-                  $599
-                </span>
-              </div>
-              <div className="flex items-center justify-between mt-4">
-                <a
-                  href="#"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Rent
-                </a>
-                <span className="text-3xl font-bold text-gray-900 dark:text-white">
-                  $99/Month
-                </span>
-              </div>
-            </div>
-          </div>
-
-          <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
-              <img
-                className="p-8 rounded-t-lg"
-                src="https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
-                alt="product image"
-              />
-            </a>
-            <div className="px-5 pb-5">
-              <a href="#">
-                <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                  Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
-                </h5>
-              </a>
-              <div className="flex items-center mt-2.5 mb-5"></div>
-              <div className="flex items-center justify-between">
-                <a
-                  href="#"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Buy
-                </a>
-                <span className="text-3xl font-bold text-gray-900 dark:text-white">
-                  $599
-                </span>
-              </div>
-              <div className="flex items-center justify-between mt-4">
-                <a
-                  href="#"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Rent
-                </a>
-                <span className="text-3xl font-bold text-gray-900 dark:text-white">
-                  $99/Month
-                </span>
-              </div>
-            </div>
-          </div>
-
-          <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
-              <img
-                className="p-8 rounded-t-lg"
-                src="https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
-                alt="product image"
-              />
-            </a>
-            <div className="px-5 pb-5">
-              <a href="#">
-                <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                  Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
-                </h5>
-              </a>
-              <div className="flex items-center mt-2.5 mb-5"></div>
-              <div className="flex items-center justify-between">
-                <a
-                  href="#"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Buy
-                </a>
-                <span className="text-3xl font-bold text-gray-900 dark:text-white">
-                  $599
-                </span>
-              </div>
-              <div className="flex items-center justify-between mt-4">
-                <a
-                  href="#"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Rent
-                </a>
-                <span className="text-3xl font-bold text-gray-900 dark:text-white">
-                  $99/Month
-                </span>
-              </div>
-            </div>
-          </div>
+        <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0 text-center">
+          <ul className="center">
+          {
+            productDetails.map(
+              (product) =>
+                <Product
+                  key={product.id}
+                  product_name={product.product_name}
+                  buy_value={product.buy_value}
+                  image_link={product.image_link}
+                />
+            )
+          }
+          </ul>
         </div>
-        
-          <CreateButton />
-  
+
+        <CreateButton />
+
       </div>
     </section>
   );
